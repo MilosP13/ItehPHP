2e23a6e68824fee40a6b494a9f21bd781944e0d4
MilosP13 <milospavlovicsd@gmail.com>
2021-11-11 14:09:04 +0100

    kreirana pocetna strana

---
+++ pocetna.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autoskola EZ</title>
+    <link rel="stylesheet" href="stylePocetna.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <section id="Logo">
+                <h1><span class="logoEZ">EASY</span></h1>
+            </section>
+        <nav>
+            <ul>
+                <li class="trenutna"><a href="pocetna.php"> Pocetna </a></li>
+                <li><a href="#">O nama</a></li>
+                <li><a href="#">Dozvole</a>
+                    <ul>
+                        <li><a href="#">A Kategorija</a></li>
+                        <li><a href="#">B Kategorija</a></li>
+                        <li><a href="#">C Kategorija</a></li>
+                    </ul>
+                </li>
+                
+                <li><a href="#">Galerija</a></li>
+                <li><a href="#">Log In</a></li>
+            </ul>
+        </nav>
+        </div>   
+    </header>
+<section id="mid">
+    <div class="container">
+        <h1>POLOZITE ZA VASU PRVU VOZACKU DOZVOLU KOD NAS!</h1>
+    </div>
+</section>
+    <footer>
+        <p>AutoskolaEASY, Smederevo, Starina Novaka 1, Copyright &copy; 2021</p>
+    </footer>
+
+</body>
+</html>
